d93dc71dcb22e7f36a5c961487d3a5922178c485
BejPastan <s.synaszko@gmail.com>
2023-10-28 20:10:48 +0200

    Start
    again... eh....

--- Assets/Scripts/MapControler.cs
+++ Assets/Scripts/MapControler.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class MapControler : MonoBehaviour
@@ -79,15 +80,116 @@ public interface IMap
 
 public class Map : IMap
 {
-    private MapTile[] map;
+    public MapTile[,] map;
+    public int rows; // Number of rows in the map.
+    public int cols; // Number of columns in the map.
+    public int mineCount; // Number of mines in the map.
+
+    public float mineChance;
+
+    public Map() { }
 
     public void CreateNewRow()
     {
+        // Create a new row at the top of the map.
+        MapTile[,] newMap = new MapTile[rows + 1, cols];
 
+        // Initialize the new row with MapTile objects.
+        for (int i = 0; i < cols; i++)
+        {
+            newMap[0, i] = CreateNewMapTile();
+        }
+
+        // Copy the existing map into the new map, shifting it down.
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                newMap[i + 1, j] = map[i, j];
+            }
+        }
+
+        // Update the map with the new row.
+        map = newMap;
+        rows++;
+
+        // Update adjacent tiles with the count of mines around them.
+        UpdateAdjacentTileCounts(0);
+    }
+
+    private MapTile CreateNewMapTile()
+    {
+        MapTile newTile = ScriptableObject.CreateInstance<MapTile>();
+
+        if(UnityEngine.Random.Range(0, 1)<mineChance)
+        {
+            newTile.ContainsMine = true;
+        }
+        // Initialize the new tile as needed, e.g., set its state, check for mines, etc.
+        return newTile;
+    }
+
+    private void UpdateAdjacentTileCounts(int row)
+    {
+        for (int i = 0; i < cols; i++)
+        {
+            MapTile currentTile = map[row, i];
+            if (!currentTile.ContainsMine)
+            {
+                int nearbyMines = CountMinesAroundTile(row, i);
+                currentTile.NearbyMinesCount = nearbyMines;
+            }
+        }
+    }
+
+    private int CountMinesAroundTile(int row, int col)
+    {
+        int mineCount = 0;
+
+        // Check the eight adjacent tiles for mines.
+        for (int i = -1; i <= 1; i++)
+        {
+            for (int j = -1; j <= 1; j++)
+            {
+                int newRow = row + i;
+                int newCol = col + j;
+
+                if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols)
+                {
+                    if (map[newRow, newCol].ContainsMine)
+                    {
+                        mineCount++;
+                    }
+                }
+            }
+        }
+
+        return mineCount;
     }
 
     public void RemoveLastRow()
     {
+        if (rows <= 1)
+        {
+            // Cannot remove the last row.
+            return;
+        }
 
+        // Remove the last row from the map.
+        MapTile[,] newMap = new MapTile[rows - 1, cols];
+
+        for (int i = 0; i < rows - 1; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                newMap[i, j] = map[i, j];
+            }
+        }
+
+        map = newMap;
+        rows--;
+
+        // Update the row above the removed row to meet Minesweeper rules.
+        UpdateAdjacentTileCounts(rows - 1);
     }
 }
--- Assets/Scripts/MapTile.cs
+++ Assets/Scripts/MapTile.cs
@@ -1,44 +1,78 @@
 using UnityEngine;
 
-public enum TileAction
-{
-    None,
-    Flag,
-    Question,
-    Uncover
-}
 
+[CreateAssetMenu(fileName = "MapTile", menuName = "Minesweeper/Map Tile")]
 public class MapTile : ScriptableObject
 {
-    private int mineNum;
-    private bool mine;
-    private TileAction action;
-
-    public int MineNum => mineNum;
-    public bool IsMine => mine;
-    public TileAction Action => action;
-
-    public void SetMine(int mineNumber)
+    public enum TileState
     {
-        mine = true;
-        mineNum = mineNumber;
-        action = TileAction.None;
+        Covered,
+        Uncovered,
+        Flagged,
+        QuestionMark
     }
 
-    public void ClearMine()
+    [SerializeField] private TileState currentState = TileState.Covered;
+    [SerializeField] private bool containsMine = false;
+    [SerializeField] private int nearbyMinesCount = 0;
+
+    public TileState CurrentState
     {
-        mine = false;
-        mineNum = 0;
-        action = TileAction.None;
+        get { return currentState; }
+        set { currentState = value; }
     }
 
-    public void SetAction(TileAction tileAction)
+    public bool ContainsMine
     {
-        action = tileAction;
+        get { return containsMine; }
+        set { containsMine = value; }
     }
 
-    public void ClearAction()
+    public int NearbyMinesCount
     {
-        action = TileAction.None;
+        get { return nearbyMinesCount; }
+        set { nearbyMinesCount = value; }
+    }
+
+    public bool IsUncovered
+    {
+        get { return currentState == TileState.Uncovered; }
+    }
+
+    public bool IsFlagged
+    {
+        get { return currentState == TileState.Flagged; }
+    }
+
+    public bool IsQuestionMarked
+    {
+        get { return currentState == TileState.QuestionMark; }
+    }
+
+    public void Uncover()
+    {
+        if (currentState == TileState.Covered)
+        {
+            currentState = TileState.Uncovered;
+            // Perform any additional game logic when a tile is uncovered.
+        }
+    }
+
+    public void ToggleFlag()
+    {
+        if (currentState == TileState.Covered)
+        {
+            currentState = IsFlagged ? TileState.Covered : TileState.Flagged;
+            // Toggle flag on the tile.
+        }
+    }
+
+    public void ToggleQuestionMark()
+    {
+        if (currentState == TileState.Covered)
+        {
+            currentState = IsQuestionMarked ? TileState.Covered : TileState.QuestionMark;
+            // Toggle question mark on the tile.
+        }
     }
 }
