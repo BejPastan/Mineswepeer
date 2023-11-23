8c1185b4b2ca51b00a742ae8e4ea96c64e0eddd9
BejPastan <s.synaszko@gmail.com>
2023-11-23 15:44:42 +0100

    pathfinding

--- Assets/Scripts/Pathfinding.cs
+++ Assets/Scripts/Pathfinding.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public static class Pathfinding
 {
     //musi jeszcze oddawać trasę
-    public static bool GetWay(int Xpos, int Ypos,ref Tile[,] map)
+    public static bool GetWay(int StartX, int StartY, int EndX, int EndY,ref Tile[,] map)
     {
-
+        //write pathfinding function in Unity C#. Your map is given as 2D array of type Tile and is named map. Tile have public function Revel(), and you can move only on Tile which Revel() return true. Start position is given in two ints StartX and StartY, the same with target position is give in EndX and EndY ints
         return false;
     }
 }
